b006ecce31e192280abc2e49a752fad6131c8eb4
Tejas <tejaswablue.1999@gmail.com>
2024-01-08 08:41:14 +0530

    Terraform config for Minikube

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resourcenamemetadeployment" {
+  type = string
+  description = "value of deployment name"  
+}
+
+variable "resourcenamemeta" {
+  type = string
+  description = "value of resource name"  
+}
